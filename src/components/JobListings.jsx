3c649f844c37ede6e5f324337b05c987703d9393
Blessing Abayomi <blessingabayomi0204@gmail.com>
2024-08-30 19:21:56 +0100

    Include prop-types validation

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,3 +1,4 @@
+import { PropTypes } from 'prop-types';
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
 import Spinner from './Spinner';
@@ -44,3 +45,7 @@ const JobListings = ({ isHome = false }) => {
 };
 
 export default JobListings;
+
+JobListings.propTypes = {
+  isHome: PropTypes.bool,
+};
